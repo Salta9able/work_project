223a4136bffcb8de6a4327b5c709d932c4c9b4f8
Saltanashka <galievasaltanat@gmail.com>
2019-07-31 00:23:15 +0600

    new parcer function added

---
+++ parcer/parcer.js
@@ -0,0 +1,37 @@
+function parcer(value) {
+  let value = `Product Name	Madinat Jumeirah - Mina A Salam   
+Starting From	21-Aug-2019 to 28-Aug-2019
+Room Type	ARABIAN DELUXE CLUB EXECUTIVE/PREMIUM LEISURE
+Meal Plan	BB`;
+
+  let arrayFull = value.split("\n");
+
+  let newArr = [];
+
+  let hotel =
+    arrayFull[0]
+      .substring(12, arrayFull[0].length + 1)
+      .trim()
+      .toUpperCase() + " - ";
+
+  let period =
+    arrayFull[1]
+      .substring(13, arrayFull[1].length + 1)
+      .trim()
+      .replace(/-/g, "")
+      .replace(/2019/g, "")
+      .replace(/ to /, "-")
+      .toUpperCase() + "\n";
+
+  let room = arrayFull[2].substring(9, arrayFull[2].length + 1).trim() + " ";
+
+  let mealPlan = arrayFull[3].substring(9, arrayFull[2].length + 1).trim();
+
+  newArr.push(period);
+  newArr.push(hotel);
+  newArr.push(room);
+  newArr.push(mealPlan);
+
+  let finalStr = newArr.join("");
+  return finalStr;
+}
