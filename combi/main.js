61be417d58c6d7ace78229f2842a71bb59c6f358
Saltanashka <galievasaltanat@gmail.com>
2019-08-03 01:11:47 +0600

    working on Combi part

---
+++ combi/main.js
@@ -0,0 +1,9 @@
+function getId(id) {
+    return document.getElementById(id);
+};
+
+//deal with hotel1 
+let button1 = getId('addBtn1');
+let demo1 = getId('demoHotel1');
+let hotel1Name = getId('hotel1Name');
+let hotel1Price = getId('hotel1Price');
